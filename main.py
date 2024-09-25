6cceadbf7879a915014651424317eb5e9fff4c48
高碩辰 <88338776+mato1321@users.noreply.github.com>
2024-09-25 16:49:35 +0800

    update .idea

--- main.py
+++ main.py
@@ -24,9 +24,10 @@ def linebot():
             message = json_data['events'][0]['message']['text']         # 取得訊息文字
             if message =='章祖綸':
                 img_url = 'https://instagram.ftpe7-1.fna.fbcdn.net/v/t51.29350-15/277795588_1330595974107864_9164530998079126436_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE2NTMuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=instagram.ftpe7-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=Bs4NORQG2o0Q7kNvgFTLyhn&edm=ACpohRwBAAAA&ccb=7-5&ig_cache_key=MjgwNTMxNjM3ODk3ODIwNzQ0Ng%3D%3D.3-ccb7-5&oh=00_AYB69C1x0xeZjDHC8FIZYeoun1_gcgmRfUmJ0R48WlPYzg&oe=66F9ACE2&_nc_sid=2d3a3f'
-                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url) #
-                linebot_api.reply_message(token, img_message)                                           #
+                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url) #發圖片
+                linebot_api.reply_message(token, img_message)                                           #回傳訊息
             linebot_api.reply_message(token, TextSendMessage(message))  # 回傳訊息
+
         elif json_data['events'][0]['message']['type'] == 'sticker':
             package = json_data['events'][0]['message']['packageId']    # 識別貼圖包的ID
             sticker = json_data['events'][0]['message']['stickerId']    # 識別貼圖的ID
