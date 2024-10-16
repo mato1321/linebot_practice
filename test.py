ac35258bb6e3a5f0b93afe2e2f9a3bd859dc033f
高碩辰 <88338776+mato1321@users.noreply.github.com>
2024-10-16 18:18:29 +0800

    e

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+import matplotlib.pyplot
+import pandas as pd
 import json
+
+with open('C:\Coding\DataVisualization_report\people_database.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+database = pd.DataFrame(data)
 
